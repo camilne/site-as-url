055d0101828c18ba14cb0f68b8ad113e812c5c67
camilne <cameronm213@gmail.com>
2018-07-07 15:00:48 -0600

    Change routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { EditorComponent } from './editor/editor.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', component: EditorComponent }
+  { path: '', pathMatch: 'full', component: EditorComponent },
+  { path: 'site-as-url', redirectTo: '' }
 ];
 
 @NgModule({
