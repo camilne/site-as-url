76ce099220f301ca104550dbbf48c5419aae53f5
camilne <cameronm213@gmail.com>
2018-07-07 15:11:39 -0600

    Change base href

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { EditorComponent } from './editor/editor.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', component: EditorComponent },
-  { path: 'site-as-url', redirectTo: '' }
+  { path: '', redirectTo: 'site-as-url', pathMatch: 'full' },
+  { path: 'site-as-url', component: EditorComponent }
 ];
 
 @NgModule({
